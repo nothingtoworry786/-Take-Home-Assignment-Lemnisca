498db2f6571099c6d2d6284891a1aa736c98bc12
Mohammed Fazil Momin <mominmasty1214@gmail.com>
2026-02-22 18:38:03 +0530

    fix port

--- backend/main.py
+++ backend/main.py
@@ -98,7 +98,7 @@ if __name__ == "__main__":
     import uvicorn
     uvicorn.run(
         "main:app",
-        host="0.0.0.0",
+        host="127.0.0.1",
         port=Config.PORT,
         reload=False,
     )
